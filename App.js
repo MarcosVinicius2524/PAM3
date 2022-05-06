9cd8561df23297b831053734f2d58522c6a32c57
MarcosVinicius2524 <marcosviniciusrc24@gmail.com>
2022-05-06 16:49:21 -0300

    adiçao dos destaques

---
+++ App.js
@@ -0,0 +1,107 @@
+import {View,Image,Text,TouchableOpacity,StyleSheet, FlatList} from 'react-native'
+import React from 'react';
+import {Ionicons} from '@expo/vector-icons';
+import Cabecalho from './src/components/Cabecalho/Cabecalho';
+import Botao from './src/components/botoes/Index';
+import { FontAwesome } from '@expo/vector-icons';
+import Jogos from './src/components/Jogo';
+import Dados from './src/Dados/dadosJogos';
+
+
+
+export default function App(){
+
+    return(
+       <View>
+           <Cabecalho>
+             
+                
+           </Cabecalho> 
+
+           <Botao
+           Logo="game-controller"
+           texto="Jogos"
+           cor="green"
+           Logo2="apps"
+           texto2="Aplicativos"
+           cor2="#295cd1"
+           
+            />         
+        
+        <Botao
+           Logo="book"
+           texto="Livros"
+           cor="#DAA520"
+           Logo2="film"
+           texto2="Filmes"
+           cor2="#D81F26"
+           
+            />
+            <Botao
+           Logo="cart"
+           texto="Loja"
+           cor="orange"
+           Logo2="cog"
+           texto2="Configurações"
+           cor2="gray"
+           
+            />
+        <FlatList
+        horizontal = {true}
+        data = {Dados}
+        keyExtractor = {(item)=> item.id}
+        renderItem = {({item}) => (
+
+        <Jogos
+        titulo = {item.nome}
+        imagem = {item.imagem}
+        valor = {item.valor} 
+        />
+        )}  
+
+        />
+
+       </View>
+
+    );
+  }
+
+  const estilo = StyleSheet.create( {
+
+      header: {
+
+          backgroundColor:'#002333',
+
+          borderRadius:2,
+
+          marginTop:8,
+
+          width:'99,9%',
+
+          padding:20,
+
+          margin:2,
+
+          alignItems:'center',
+
+          flexDirection:'row',
+
+          justifyContent: 'space-between',
+
+          marginBottom:20
+
+      },
+
+      Texto:{
+
+          color:'white',
+
+          marginLeft:-120,
+
+          fontSize:20,
+
+          fontWeight:'bold'
+
+      }
+
+  })
---
+++ src/components/Jogo/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {View, TouchableOpacity, StyleSheet, Text, Image } from "react-native";
+
+//Criando a função e recebendo por props o titulo valor e imagem do jogo
+export default function Jogos({titulo, valor, imagem}) {
+    
+    
+    return (
+        <TouchableOpacity style={estilo.containerJogos}>
+            <Image
+            style={estilo.images}
+                source={require(`../../imagens/${imagem}`)}
+         />
+
+         <Text style={estilo.titulo}> {titulo} </Text>
+         <Text style={estilo.valor}> {valor} </Text>
+         </TouchableOpacity>
+    );
+
+}
+
+const estilo = StyleSheet.create({
+   containerJogos: {
+    backgroundColor: "#DCDCDC",
+    borderRadius: 10,
+    marginTop: 8,
+    padding: 10,
+    margin: 2,
+    alighItems: "center",
+    justifyContent: "space-between",
+    width: 100,
+    height: 150,
+    marginLeft: 8,
+
+   },
+
+   titulo:{
+    color: "black",
+    fontSize: 12,
+    fontWeight: "bold",
+
+   },
+valor:{
+    color:"green",
+    fontSize: 12,
+    marginTop: 29,
+    fontWeight: "bold",
+
+},
+
+images: {
+    width: "100%",
+    height: 75,
+    borderRadius: 3,
+
+},
+
+});
